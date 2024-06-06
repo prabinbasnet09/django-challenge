b89599ea5329369b361eb38bf793e2e031b8186c
Prabin Basnet <prabinbasnet09@gmail.com>
2024-06-06 16:59:54 -0500

    adds a detail view for movies

--- coding_challenge/movies/urls.py
+++ coding_challenge/movies/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
 from movies.views import MovieListView
+from movies.views import MovieDetailView
 
 urlpatterns = [
     path("", MovieListView.as_view(), name="MovieListView"),
+    path("<int:pk>/",MovieDetailView.as_view(), name="MovieDetailView"),
 ]
