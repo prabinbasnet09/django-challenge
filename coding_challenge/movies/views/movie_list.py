cb7e6babd61c6f10e52eddbeda8375b793c2a5a5
Prabin Basnet <prabinbasnet09@gmail.com>
2024-06-06 18:03:02 -0500

    adds query params to list view for filtering on runtime

--- coding_challenge/movies/views/movie_list.py
+++ coding_challenge/movies/views/movie_list.py
@@ -5,5 +5,16 @@ from movies.serializers import MovieSerializer
 
 
 class MovieListView(ListCreateAPIView):
-    queryset = Movie.objects.order_by("id")
     serializer_class = MovieSerializer
+
+    def get_queryset(self):
+        runtime = self.request.query_params.get("runtime")
+        runtime_lt = self.request.query_params.get("runtime_lt")
+        runtime_gt = self.request.query_params.get("runtime_gt")
+
+        if runtime:
+            return Movie.objects.filter(runtime=runtime).order_by("id")
+        if runtime_lt:
+            return Movie.objects.filter(runtime__lt=runtime_lt).order_by("id")
+        if runtime_gt:
+            return Movie.objects.filter(runtime__gt=runtime_gt).order_by("id")
