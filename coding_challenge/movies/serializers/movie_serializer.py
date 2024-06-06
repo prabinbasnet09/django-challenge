9f51ae6c80774b06928b73ce3899df9932a3307d
Prabin Basnet <prabinbasnet09@gmail.com>
2024-06-06 17:03:45 -0500

    adds a runtime_formatted field to the API

--- coding_challenge/movies/serializers/movie_serializer.py
+++ coding_challenge/movies/serializers/movie_serializer.py
@@ -4,6 +4,7 @@ from movies.models import Movie
 
 
 class MovieSerializer(serializers.ModelSerializer):
+    runtime_formatted = serializers.SerializerMethodField()
     class Meta:
         model = Movie
         fields = (
@@ -11,4 +12,9 @@ class MovieSerializer(serializers.ModelSerializer):
             "title",
             "runtime",
             "release_date",
+            "runtime_formatted"
         )
+    
+    def get_runtime_formatted(self, obj):
+        hours, minutes = divmod(obj.runtime, 60)
+        return f"{hours}:{minutes:02d}"
