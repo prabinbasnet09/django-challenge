3c91ab713796d4e8dfff6e90f19b93116f8a8037
Prabin Basnet <prabinbasnet09@gmail.com>
2024-06-06 17:54:16 -0500

    adds avg_rating field to the api

--- coding_challenge/movies/models/movie.py
+++ coding_challenge/movies/models/movie.py
@@ -5,3 +5,10 @@ class Movie(models.Model):
     title = models.CharField(max_length=100, unique=True)
     runtime = models.PositiveIntegerField()
     release_date = models.DateField()
+
+    def get_avg_rating(self):
+        reviewers = self.reviewers.all()
+        if not reviewers:
+            return None
+        return round(reviewers.aggregate(models.Avg("rating"))["rating__avg"], 2)
+        
--- coding_challenge/movies/serializers/movie_serializer.py
+++ coding_challenge/movies/serializers/movie_serializer.py
@@ -7,6 +7,7 @@ from movies.serializers.review_serializer import ReviewSerializer
 class MovieSerializer(serializers.ModelSerializer):
     runtime_formatted = serializers.SerializerMethodField()
     reviewers = ReviewSerializer(many=True, read_only=True)
+    avg_rating = serializers.SerializerMethodField()
     class Meta:
         model = Movie
         fields = (
@@ -15,9 +16,13 @@ class MovieSerializer(serializers.ModelSerializer):
             "runtime",
             "release_date",
             "runtime_formatted",
-            "reviewers"
+            "reviewers",
+            "avg_rating",
         )
     
     def get_runtime_formatted(self, obj):
         hours, minutes = divmod(obj.runtime, 60)
         return f"{hours}:{minutes:02d}"
+    
+    def get_avg_rating(self, obj):
+        return obj.get_avg_rating()
